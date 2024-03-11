146bd522d42932248a8fb527d97e9ed339b9dd57
Haoyu Liang <hliang11@sheffield.ac.uk>
2024-03-11 16:57:21 +0000

    Task 1 answer: # The essential factors of software testing include: Correctness,Reliability,Performance,Security,Usability,Compatibility,Maintainability,Portability

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,3 +11,7 @@ class MyTestCase(unittest.TestCase):
 
     # def test_complex_function(self):
     #     self.assertEqual('behaviour 1', complex_function())
+    # Simulation: Simulating a dependent function, such as a random function, in order to control its return value, allowing various cases to be tested deterministically.
+    # Parameter injection: Enables deterministic testing by modifying the function to accept additional parameters to control the test behavior.
+    # Statistical testing: Run the test multiple times and use statistical methods to verify that the resulting distribution is as expected.
+    # Behavioral testing: Focusing on the impact or resulting state of a function rather than its direct output.
