91ebe62b667bea9d1fecc444d19dca647b79f191
Haoyu Liang <hliang11@sheffield.ac.uk>
2024-03-11 16:58:50 +0000

    Task 3: Simulation: Simulating a dependent function, such as a random function, in order to control its return value, allowing various cases to be tested deterministically. Parameter injection: Enables deterministic testing by modifying the function to accept additional parameters to control the test behavior. Statistical testing: Run the test multiple times and use statistical methods to verify that the resulting distribution is as expected. Behavioral testing: Focusing on the impact or resulting state of a function rather than its direct output.

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,6 +11,8 @@ class MyTestCase(unittest.TestCase):
 
     # def test_complex_function(self):
     #     self.assertEqual('behaviour 1', complex_function())
+
+
     # Simulation: Simulating a dependent function, such as a random function, in order to control its return value, allowing various cases to be tested deterministically.
     # Parameter injection: Enables deterministic testing by modifying the function to accept additional parameters to control the test behavior.
     # Statistical testing: Run the test multiple times and use statistical methods to verify that the resulting distribution is as expected.
