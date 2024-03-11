04f6c439935d21a5429e6150113f0e10e2d98c63
Haoyu Liang <hliang11@sheffield.ac.uk>
2024-03-11 16:47:39 +0000

    Task 1 answer: # The essential factors of software testing include: Correctness,Reliability,Performance,Security,Usability,Compatibility,Maintainability,Portability

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ def simple_count(s):
     return len(s)  # Correctly returns the length of the string
 
 
+# The essential factors of software testing include:
+# Correctness
+# Reliability
+# Performance
+# Security
+# Usability
+# Compatibility
+# Maintainability
+# Portability
+
 def random_function():
     import random
     if random.random() < 0.001:
